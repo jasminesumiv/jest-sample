647c90a45bc7f668cc376cec02b9973ff199a1ba
Jasmine <jasminesumi1997@gmail.com>
2022-11-26 15:01:56 +0530

    trialcommit

--- Welcome.js
+++ Welcome.js
@@ -9,6 +9,7 @@ const Welcome = () => {
   return (
     <div>
       <h1>This is sample text</h1>
+      <h1>This is trial</h1>
       <h1>{text}</h1>
       <button onClick={textChange}>Change</button>
     </div>
